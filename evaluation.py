a768066b2a7f6a275638fa64bf34e1fba9e409ce
Виктория Князькова <vknyazkova01@gmail.com>
2024-05-25 19:21:11 +0300

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -1,9 +1,12 @@
 from chromadb import Documents, EmbeddingFunction, Embeddings
 
 from tangent_cft_back_end import TangentCFTBackEnd
+from tangent_cft_encoder import FormulaTreeEncoder
+from tangent_cft_module import TangentCFTModule
 from tangent_cft_parser import TangentCFTParser
 from Configuration.configuration import Configuration
 from Embedding_Preprocessing.encoder_tuple_level import TupleTokenizationMode
+
 
 class TangentCFTEmbedding(EmbeddingFunction[Documents]):
     def __init__(self,
@@ -29,17 +32,13 @@ if __name__ == '__main__':
 
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
 
-    config = Configuration('Configuration/config/config_1')
-    system = TangentCFTBackEnd(ft_config=config,
-                               data_reader=None,
-                               embedding_type=TupleTokenizationMode(3),
-                               ignore_full_relative_path=True,
-                               tokenize_all=False,
-                               tokenize_number=True)
-    system.load_model(
-        encoder_map_path='Embedding_Preprocessing/slt_encoder.tsv',
-        ft_model_path='FT_models/slt_model.model'
-    )
+
+    encoder = FormulaTreeEncoder.load('Models/Vocabulary/slt_encoder.tsv')
+
+    ft_config = Configuration('Configuration/config/config_1')
+    module = TangentCFTModule(ft_config, 'Models/FastText/slt_model.model')
+
+    system = TangentCFTBackEnd(tangent_cft_module=module, encoder=encoder)
     embedder = TangentCFTEmbedding(tangent_cft=system, mathml=False, slt=True)
 
     latex_formulas = [
