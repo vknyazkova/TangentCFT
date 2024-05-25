b2c67c3aefff38148c6baa929b32f13ff852bb5c
Виктория Князькова <vknyazkova01@gmail.com>
2024-05-25 21:00:26 +0300

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -1,4 +1,4 @@
-from chromadb import Documents, EmbeddingFunction, Embeddings
+from chromadb import Documents, EmbeddingFunction, Embeddings, Client
 
 from tangent_cft_back_end import TangentCFTBackEnd
 from tangent_cft_encoder import FormulaTreeEncoder
@@ -33,18 +33,60 @@ if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
 
 
-    encoder = FormulaTreeEncoder.load('Models/Vocabulary/slt_encoder.tsv')
+    encoder = FormulaTreeEncoder.load('Models/Vocabulary/opt_encoder.tsv')
 
     ft_config = Configuration('Configuration/config/config_1')
-    module = TangentCFTModule(ft_config, 'Models/FastText/slt_model.model')
+    module = TangentCFTModule(ft_config, 'Models/FastText/opt_model.model')
 
     system = TangentCFTBackEnd(tangent_cft_module=module, encoder=encoder)
     embedder = TangentCFTEmbedding(tangent_cft=system, mathml=False, slt=True)
 
-    latex_formulas = [
-        '\\frac{\\partial \\mathbf{x}}{\\partial x}',
-        '\\frac{x}{2}',
-        '(x+2)^2'
-    ]
-    embeddings = embedder(latex_formulas)
-    print(embeddings)
+    qualitive_dataset = {
+        '1:a': '(a+b)\\times(c+d)',
+        '1:b': '\\frac{1+2}{3+4}',
+        '1:c': '(a+b)/(c+d)',
+        '1:d': '(a+b) \\div (c+d)',
+        '1:e': '(1+2) \\times (3+4)',
+        '1:f': '\\frac{a+b}{c}',
+        '1:g': '\\frac{a+b}{c+d}',
+        '2:a': 'a*b',
+        '2:b': 'a\\times b',
+        '2:c': 'a \\cdot b',
+        '2:d': 'ab',
+        '2:e': '5\\cdot 6',
+        '2:f': 'a \\div b',
+        '3:a': '\\int_a^b h(x) dx',
+        '3:b': '\\int_a^b v(t) dt',
+        '3:c': '\\int g(x) dx',
+        '3:d': '\\int_{-2}^5 g(x)dx',
+        '3:e': 'g(x)',
+        '3:f': '\\int_a^b g(x) dx',
+        '4:a': '\\int_0^1 g(t) dt',
+        '4:b': '\\sum_{i=1}^n a_i',
+        '4:c': '\\frac{d}{dx} f(x)',
+        '5:a': 'E = \\frac{1}{2} mv^2',
+        '5:b': 'E = hv',
+        '5:c': 'F = ma',
+        '5:d': 'pV = nRT',
+        '6:a': 'e^{ix} = cos(x) + i\\ sin(x)',
+        '6:b': 'e^x = \\sum_{n=0}^{\infin} \\frac{x^n}{n!}',
+        '6:c': 'ln(e) = 1'
+    }
+    queries = {
+        '1': '\\frac{a+b}{c+d}',
+        '2': 'ab',
+        '3': '\\int_a^b g(x) dx',
+        '4': '\\int_a^b f(x) dx',
+        '5': 'E=mc^2',
+        '6': 'e^{i\\pi} + 1 = 0',
+    }
+
+    chroma_client = Client()
+    collection = chroma_client.create_collection(name="my_collection", embedding_function=embedder)
+
+    collection.add(
+        documents=list(qualitive_dataset.values()),
+        ids=list(qualitive_dataset.keys())
+    )
+    results = collection.query(query_texts=list(queries.values()))
+    print(results['documents'])
