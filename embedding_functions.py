4bea0f2411256f3a290094d919ab9232274125f6
Виктория Князькова <vknyazkova01@gmail.com>
2024-05-28 16:01:40 +0300

    add evaluation

---
+++ embedding_functions.py
@@ -0,0 +1,33 @@
+import logging
+
+from chromadb import Documents, EmbeddingFunction, Embeddings
+from tqdm import tqdm
+import numpy as np
+
+from tangent_cft_parser import TangentCFTParser
+from tangent_cft_back_end import TangentCFTBackEnd
+
+
+class TangentCFTEmbedding(EmbeddingFunction[Documents]):
+    def __init__(self,
+                 tangent_cft: TangentCFTBackEnd,
+                 mathml: bool = False,
+                 slt: bool = False
+                 ):
+        self.tangent_cft = tangent_cft
+        self.tangent_cft_parser = TangentCFTParser()
+        self.mathml = mathml
+        self.slt = slt
+
+    def __call__(self, input: Documents) -> Embeddings:
+        embeds = []
+        for formula in tqdm(input):
+            try:
+                formula_tree_tuples = self.tangent_cft_parser.parse(formula, mathml=self.mathml, slt=self.slt)
+                embeds.append(self.tangent_cft.get_formula_emebedding(formula_tree_tuples).tolist())
+            except Exception as e:
+                logging.debug(f'Problems with formula {formula}')
+                embeds.append(np.random.random(300).tolist())
+        return embeds
+
+    
--- evaluation.py
+++ evaluation.py
@@ -1,47 +1,16 @@
-from chromadb import Documents, EmbeddingFunction, Embeddings, Client
+from typing import Optional, List, Dict
 
-from tangent_cft_back_end import TangentCFTBackEnd
+import pandas as pd
+from chromadb import Client, Documents, EmbeddingFunction, Embeddings
+
 from tangent_cft_encoder import FormulaTreeEncoder
 from tangent_cft_module import TangentCFTModule
-from tangent_cft_parser import TangentCFTParser
+from tangent_cft_back_end import TangentCFTBackEnd
 from Configuration.configuration import Configuration
-from Embedding_Preprocessing.encoder_tuple_level import TupleTokenizationMode
+from embedding_functions import TangentCFTEmbedding
 
 
-class TangentCFTEmbedding(EmbeddingFunction[Documents]):
-    def __init__(self,
-                 tangent_cft: TangentCFTBackEnd,
-                 mathml: bool = False,
-                 slt: bool = False
-                 ):
-        self.tangent_cft = tangent_cft
-        self.tangent_cft_parser = TangentCFTParser()
-        self.mathml = mathml
-        self.slt = slt
-
-    def __call__(self, input: Documents) -> Embeddings:
-        embeds = []
-        for formula in input:
-            formula_tree_tuples = self.tangent_cft_parser.parse(formula, mathml=self.mathml, slt=self.slt)
-            embeds.append(self.tangent_cft.get_formula_emebedding(formula_tree_tuples).tolist())
-        return embeds
-
-
-if __name__ == '__main__':
-    import logging
-
-    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
-
-
-    encoder = FormulaTreeEncoder.load('Models/Vocabulary/opt_encoder.tsv')
-
-    ft_config = Configuration('Configuration/config/config_1')
-    module = TangentCFTModule(ft_config, 'Models/FastText/opt_model.model')
-
-    system = TangentCFTBackEnd(tangent_cft_module=module, encoder=encoder)
-    embedder = TangentCFTEmbedding(tangent_cft=system, mathml=False, slt=True)
-
-    qualitive_dataset = {
+dataset = {
         '1:a': '(a+b)\\times(c+d)',
         '1:b': '\\frac{1+2}{3+4}',
         '1:c': '(a+b)/(c+d)',
@@ -72,21 +41,65 @@ if __name__ == '__main__':
         '6:b': 'e^x = \\sum_{n=0}^{\infin} \\frac{x^n}{n!}',
         '6:c': 'ln(e) = 1'
     }
-    queries = {
-        '1': '\\frac{a+b}{c+d}',
-        '2': 'ab',
-        '3': '\\int_a^b g(x) dx',
-        '4': '\\int_a^b f(x) dx',
-        '5': 'E=mc^2',
-        '6': 'e^{i\\pi} + 1 = 0',
-    }
+queries = {
+    '1': '\\frac{a+b}{c+d}',
+    '2': 'ab',
+    '3': '\\int_a^b g(x) dx',
+    '4': '\\int_a^b f(x) dx',
+    '5': 'E=mc^2',
+    '6': 'e^{i\\pi} + 1 = 0',
+}
+
+
+def index_dataset(chroma_client: Client,
+                  collection_name: str,
+                  embedding_fn: EmbeddingFunction,
+                  documents: Documents,
+                  ids: List[str],
+                  embeddings: Optional[Embeddings] = None):
+    collection = chroma_client.get_or_create_collection(name=collection_name, embedding_function=embedding_fn,
+                                                        metadata={"hnsw:space": "cosine"})
+    collection.upsert(
+        documents=documents,
+        ids=ids,
+        embeddings=embeddings,
+        )
+
+
+def run_test_queries(chroma_client: Client,
+                 collection_name: str,
+                 embedding_fn: EmbeddingFunction,
+                 queries: Dict[str, str],
+                 queries_embeds: Optional[Embeddings] = None,
+                 n_results: int = 10):
+    collection = chroma_client.get_collection(name=collection_name, embedding_function=embedding_fn)
+    res = collection.query(query_embeddings=queries_embeds, query_texts=list(queries.values()), n_results=n_results, include=['distances', 'documents'])
+    results = []
+    for i, (query_id, query) in enumerate(queries.items()):
+        for j in range(n_results):
+            results.append([
+                query_id,
+                query,
+                j + 1,
+                res['documents'][i][j]
+            ])
+    return pd.DataFrame(results, columns=['query_id', 'query', 'rank', 'formula'])
+
+
+if __name__ == '__main__':
+    import logging
+
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+    encoder = FormulaTreeEncoder.load('Models/Vocabulary/slt_type_encoder.tsv')
+    ft_config = Configuration('Configuration/config/config_3')
+    module = TangentCFTModule(ft_config, 'Models/FastText/slt_type_model.model')
+    system = TangentCFTBackEnd(tangent_cft_module=module, encoder=encoder)
 
     chroma_client = Client()
-    collection = chroma_client.create_collection(name="my_collection", embedding_function=embedder)
-
-    collection.add(
-        documents=list(qualitive_dataset.values()),
-        ids=list(qualitive_dataset.keys())
-    )
-    results = collection.query(query_texts=list(queries.values()))
-    print(results['documents'])
+    tangent_cft_embedder = TangentCFTEmbedding(tangent_cft=system, mathml=False, slt=True)
+    collection_name = 'my_test_slt_type'
+    index_dataset(chroma_client, collection_name=collection_name, embedding_fn=tangent_cft_embedder,
+                  documents=list(dataset.values()), ids=list(dataset.keys()), embeddings=None)
+    retrieved = run_test_queries(chroma_client, collection_name, tangent_cft_embedder, queries=queries, n_results=10)
+    retrieved.to_csv('/Users/viktoriaknazkova/Desktop/me/study/thesis/my_test_slt_type.tsv', header=True, index=False, sep='\t')
